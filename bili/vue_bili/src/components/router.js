2c9615035b9f16c6f5d16523a7a2d4b4796269fa
lhd <1215643818@qq.com>
2025-07-09 01:13:28 +0800

    路由设置(修改)

--- bili/vue_bili/src/components/router.js
+++ bili/vue_bili/src/components/router.js
@@ -1,13 +1,13 @@
 import {createRouter, createWebHashHistory} from "vue-router"
 import VideoPage from './videoPage'
-import MainPage from '@/components/MainPage.vue'  // 直接导入或保持动态导入
+import indexPage from "@/components/indexPage";  // 直接导入或保持动态导入
 
 const router = createRouter({
     history: createWebHashHistory(),
     routes: [
         {
             path: "/",
-            component: MainPage,  // 或者保持 () => import('@/components/MainPage.vue')
+            component: indexPage,  // 或者保持 () => import('@/components/MainPage.vue')
         },
         {
             path: "/video/:id",
